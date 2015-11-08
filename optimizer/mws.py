1e3231e4df87ab4d15f4de75142287cc851d0981
Rohan Chakravarthy <rohanc219@gmail.com>
2015-11-07 19:32:16 -0500

    Added Type1 and Type2 to sa and mws Fixed a12

--- optimizer/mws.py
+++ optimizer/mws.py
@@ -2,63 +2,131 @@ from __future__ import division
 import random
 from common import prerun
 from model.helpers.candidate import Candidate
+from helpers.a12 import a12
 
 
-def mws(model, p=0.5, threshold=0.001, max_tries=100, max_changes=10, optimal='low'):
+def mws(model, p=0.5, threshold=0.001, max_tries=100, max_changes=10, era_size=10, era0=None, lives=5):
 
     best_can = None
 
     normalize = prerun(model)
+    aggregate = model.aggregate
+
+    def n_score(can):
+        return normalize(aggregate(can))
+
+    # if energy of can1 is less than that of can2
+    # can1 is better and this returns true
+    def type1(can1, can2):
+        return (n_score(can1) < n_score(can2))
+
+    def type2(era1, era2):
+        # a12 returns times that lst1 is greater than lst2
+        total = 0
+        n = 0
+        for obj_scores1, obj_scores2 in zip(era1, era2):
+            # If this is 1, that means new one is worse
+            total += a12(obj_scores1, obj_scores2)
+            n += 1
+        return (total / n < 0.5)
+
+    best_score = 1.0
+
+    if not era0:
+        curr_era = []
+        curr_lives = lives
+
+    else:
+        # List of List. Need to deepcopy internal list too
+        curr_era = list([list(x) for x in era0])
+        best_can = curr_era[0]
+        for can in curr_era:
+            can_score = n_score(can)
+            if can_score < n_score(best_can):
+                best_can = can
+                best_score = can_score
+
+    # This stores a list of era entries, i.e a list of  [list of objective scores for every candidate in the era]
+    # Assume one era is 5 candidates
+    # One Era entry for a model with 2 objectives: [[0.5,0.5], [0.2,0.3], [0.5,0.5], [0.2,0.3], [0.5,0.5]]
+    # All era entries will be stored in eras (Assume 2 eras): [[[0.5,0.5], [0.2,0.3], [0.5,0.5], [0.2,0.3], [0.5,0.5]],
+    #                                                         [[0.5,0.5], [0.2,0.3], [0.5,0.5], [0.2,0.3], [0.5,0.5]]]
+    eras = []
 
     out = []
 
-    if optimal == 'low':
-        best_score = 1.0
-    else:
-        best_score = 0.0
+    candidate = None
 
-    for i in range(0, max_tries):
-        candidate = model.gen_candidate()
+    i = -1
+    thresh_flag = False
 
-        if i % 10 == 0:
+    while not thresh_flag:
+        i += 1
+        j = 0
+        if i == max_tries:
+            if curr_era:
+                eras += [curr_era]
+                curr_era = []
+            out += ["\nReached Max Tries"]
+            break
+
+        if i % era_size == 0:
             out += ["\n" + str(best_score) + " "]
+            if curr_era:
+                eras += [curr_era]
+                curr_era = []
+                if len(eras) > 1:
+                    last_index = len(eras) - 1
+                    # If there is improvement reset lives, else decrement
+                    if (type2(eras[last_index], eras[last_index - 1])):
+                        curr_lives = lives
+                    else:
+                        curr_lives -= 1
+                        if curr_lives == 0:
+                            out += ["\nNo more Lives"]
+                            break
+
+        if candidate is not None:
+            prev_candidate = candidate
+
+        if i == 0:
+            while candidate is None:
+                candidate = model.gen_candidate()
+        else:
+            candidate = model.gen_candidate()
 
         # could not generate a valid candidate after patience tries
         if candidate is None:
             out += ["."]
+            model.eval(prev_candidate)
+            obj_scores = [x for x in prev_candidate.scores]
+            curr_era += [obj_scores]
             continue
 
         if best_can is None:
             best_can = candidate
-            best_score = normalize(model.aggregate(candidate))
+            best_score = n_score(candidate)
 
         for j in range(0, max_changes):
             model.eval(candidate)
-            score = normalize(model.aggregate(candidate))
+            score = n_score(candidate)
 
-            if optimal == 'low':
-                if score < threshold:
-                    out += ["\niterations:" + str(i * max_changes + j)]
-                    out += ["Score:" + str(score)]
-                    print ''.join(out)
-                    return candidate, score
+            model.eval(candidate)
+            obj_scores = [x for x in candidate.scores]
+            curr_era += [obj_scores]
 
-                if score < best_score:
-                    out += ["!"]
-                    best_can = candidate
-                    best_score = score
+            if score < best_score:
+                out += ["!"]
+                best_can = candidate
+                best_score = score
 
-            else:
-                if score > threshold:
-                    out += ["iterations:" + str(i * max_changes + j)]
-                    out += ["Score:" + str(score)]
-                    print ''.join(out)
-                    return candidate, score
-
-                if score > best_score:
-                    out += ["!"]
-                    best_can = candidate
-                    best_score = score
+            if best_score < threshold:
+                if curr_era:
+                    eras += [curr_era]
+                    curr_era = []
+                out += ["\nScore satisfies threshold"]
+                thresh_flag = True
+                break
 
             # choose a random decision
             c = random.randrange(0, len(model.decs))
@@ -79,8 +147,8 @@ def mws(model, p=0.5, threshold=0.001, max_tries=100, max_changes=10, optimal='l
                     out += ["."]
 
             else:
-                orig_score = normalize(model.aggregate(candidate))
-                candidate = mws_optimize(model, candidate, c, optimal)
+                orig_score = n_score(candidate)
+                candidate = mws_optimize(model, candidate, c)
                 new_score = normalize(model.aggregate(candidate))
                 if orig_score != new_score:
                     out += ["+"]
@@ -88,14 +156,13 @@ def mws(model, p=0.5, threshold=0.001, max_tries=100, max_changes=10, optimal='l
                     out += ["."]
 
     print ''.join(out)
-
-    print "\niterations:" + str(max_changes * max_tries)
+    print "\niterations:" + str(max_changes * i + j)
     print "Best Score:" + str(normalize(model.aggregate(best_can)))
 
     return best_can, normalize(model.aggregate(best_can))
 
 
-def mws_optimize(model, candidate, dec_index, optimization, tries=50):
+def mws_optimize(model, candidate, dec_index, tries=50):
 
     best_can = candidate
     model.eval(best_can)
@@ -110,15 +177,9 @@ def mws_optimize(model, candidate, dec_index, optimization, tries=50):
         model.eval(new_can)
         score = model.aggregate(new_can)
 
-        if optimization == 'low':
-            # We want to lower score
-            if score < best_score:
-                best_score = score
-                best_can = new_can
-
-        else:
-            if score > best_score:
-                best_score = score
-                best_can = new_can
+        # We want to lower score
+        if score < best_score:
+            best_score = score
+            best_can = new_can
 
     return best_can
--- optimizer/sa.py
+++ optimizer/sa.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import random
 from common import prerun
+from helpers.a12 import a12
 import math
 
 
@@ -12,58 +13,139 @@ def sa_default_prob(curr_score, new_score, t):
     return val
 
 
-def sa(model, p=sa_default_prob, threshold=0.001, max_tries=1000, optimal='low'):
-
-    best_can = model.gen_candidate()
-    while best_can is None:
-        best_can = model.gen_candidate()
+def sa(model, p=sa_default_prob, threshold=0.001, max_tries=1000, lives=5, era_size=50, era0=None):
 
     normalize = prerun(model)
-    best_score = normalize(model.aggregate(best_can))
 
-    curr_can = best_can
-    curr_score = best_score
+    aggregate = model.aggregate
+
+    def n_score(can):
+        return normalize(aggregate(can))
+
+    # if energy of can1 is less than that of can2
+    # can1 is better and this returns true
+    def type1(can1, can2):
+        return (n_score(can1) < n_score(can2))
+
+    def type2(era1, era2):
+        # a12 returns times that lst1 is greater than lst2
+        total = 0
+        n = 0
+        for obj_scores1, obj_scores2 in zip(era1, era2):
+            # If this is 1, that means new one is worse
+            total += a12(obj_scores1, obj_scores2)
+            n += 1
+        return (total / n < 0.5)
+
+    # This stores a list of era entries, i.e a list of  [list of objective scores for every candidate in the era]
+    # Assume one era is 5 candidates
+    # One Era entry for a model with 2 objectives: [[0.5,0.5], [0.2,0.3], [0.5,0.5], [0.2,0.3], [0.5,0.5]]
+    # All era entries will be stored in eras (Assume 2 eras): [[[0.5,0.5], [0.2,0.3], [0.5,0.5], [0.2,0.3], [0.5,0.5]],
+    #                                                         [[0.5,0.5], [0.2,0.3], [0.5,0.5], [0.2,0.3], [0.5,0.5]]]
+
+    if not era0:
+        best_can = model.gen_candidate()
+        while best_can is None:
+            best_can = model.gen_candidate()
+
+        best_score = n_score(best_can)
+        curr_can = best_can
+        curr_score = best_score
+        curr_era = []
+
+    else:
+        # List of List. Need to deepcopy internal list too
+        curr_era = list([list(x) for x in era0])
+        best_can = curr_era[0]
+        for can in curr_era:
+            if n_score(can) < n_score(best_can):
+                best_can = can
+        curr_can = curr_era[len(curr_era) - 1]
+        curr_score = n_score(curr_can)
+
+    best_score = n_score(best_can)
 
     out = []
 
-    for i in range(0, max_tries):
+    eras = []
+    curr_lives = lives
+    i = -1
 
-        if i % 50 == 0:
+    # If in `lives` eras there is no improvement, EXIT
+    # If iterations > max_tries, EXIT
+    # If n_score <threshold, EXIT
+
+    while True:
+        i += 1
+
+        if i == max_tries:
+            out += ["\nReached max tries"]
+            if curr_era:
+                eras += [curr_era]
+                curr_era = []
+            break
+
+        # Beginning of a new ERA
+        if i % era_size == 0:
             out += ["\n" + str(best_score) + " "]
+            if curr_era:
+                eras += [curr_era]
+                curr_era = []
+                if len(eras) > 1:
+                    last_index = len(eras) - 1
+                    # If there is improvement reset lives, else decrement
+                    if (type2(eras[last_index], eras[last_index - 1])):
+                        curr_lives = lives
+                    else:
+                        curr_lives -= 1
+                        if curr_lives == 0:
+                            out += ["\nNo more Lives"]
+                            break
 
         new_can = model.gen_candidate()
         if new_can is None:
             out += ["."]
+            model.eval(curr_can)
+            obj_scores = [x for x in curr_can.scores]
+            curr_era += [obj_scores]
             continue
-        new_score = normalize(model.aggregate(new_can))
 
-        if optimal == 'low':
-            flag = True
-            if new_score < best_score:
-                best_score = new_score
-                best_can = new_can
-                out += ["!"]
-                flag = False
+        model.eval(new_can)
+        obj_scores = [x for x in new_can.scores]
+        curr_era += [obj_scores]
 
-            if new_score < curr_score:
-                curr_score = new_score
-                curr_can = new_can
-                out += ["+"]
-                flag = False
+        new_score = n_score(new_can)
 
-            elif p(curr_score, new_score, i / max_tries) < random.random():
-                curr_score = new_score
-                curr_can = new_can
-                out += ["?"]
-                flag = False
+        flag = True
+        if new_score < best_score:
+            best_score = new_score
+            best_can = new_can
+            out += ["!"]
+            flag = False
 
-            if best_score < threshold:
-                break
+        if new_score < curr_score:
+            curr_score = new_score
+            curr_can = new_can
+            out += ["+"]
+            flag = False
 
-            if flag is True:
-                out += "."
+        elif p(curr_score, new_score, i / max_tries) < random.random():
+            curr_score = new_score
+            curr_can = new_can
+            out += ["?"]
+            flag = False
+
+        if best_score < threshold:
+            out += ["\nScore satisfies Threshold"]
+            break
+
+        if flag is True:
+            out += "."
+
+    if curr_era:
+        eras += [curr_era]
 
     print ''.join(out)
-    print "\niterations:" + str(i + 1)
+    print "\niterations:" + str(i)
     print "Score:" + str(best_score)
     return best_can, best_score
